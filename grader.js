dbd3644479b331460c10c2c1ce8cc6e2584b0b0e
Rajinder Gupta <rajinder001@gmail.com>
2013-07-15 18:54:30 +0000

    modified code to check url using restler utils

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +62,40 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var handleUrl = function(result, response) {
+
+    if ( result instanceof Error) {
+        //	console.error('Error: ' +util.format(response.message));
+	console.log("Error opeing the url");
+    } else {
+	var htmlfile ="htmlfile";
+	fs.writeFileSync( htmlfile, result);
+	var checkJson = checkHtmlFile(htmlfile, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+}
+
+var checkUrl = function(url){
+    rest.get(url).on('complete', handleUrl);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'htttp url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    console.log("program.url = "+program.url); 
+    
+    if ( program.url.length > 0 ) {
+	checkUrl(program.url, program.checks);
+    } else{
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
